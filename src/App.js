2fb779c90d7233b89de641ba1b830a35ad810d44
gurungbjta <bijayata.gurung@sjsu.edu>
2022-07-05 17:39:44 -0700

    reset

--- src/App.js
+++ src/App.js
@@ -32,7 +32,6 @@ function App() {
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Pagination Posts</h1>
-      <h2>Just</h2>
       <Posts posts={currPosts} loading={loading} />
       <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} changePageNum={changePageNum} />
     </div>
