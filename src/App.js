2e72ffdcc1213af3221b0a34eeb5eb62f55c754a
gurungbjta <bijayata.gurung@sjsu.edu>
2022-07-06 17:01:43 -0700

    test 12

--- src/App.js
+++ src/App.js
@@ -32,9 +32,8 @@ function App() {
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Pagination Posts</h1>
-      <div>hello</div>
-      <div>test 10 I</div>
-      <div>test 10 II</div>
+      <div>test 12 I</div>
+
       <Posts posts={currPosts} loading={loading} />
       <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} changePageNum={changePageNum} />
     </div>
